5d745017acc74728160c521facbb4165b57ff92f
Mauro Vera <mau4250@gmail.com>
2013-04-15 17:39:32 -0400

    Cambios pequeños

--- sgp/config.py
+++ sgp/config.py
@@ -3,6 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-
+#Comentario al pedo
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:sgp@localhost/sgp'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
