373624f112df18711e73a24cad3a581b975b3bb5
Mauro Vera <mau4250@gmail.com>
2013-05-08 11:29:49 -0400

    implementacion caducar dato e invertir. cambios menores

--- sgp/app/vista/itemView.py
+++ sgp/app/vista/itemView.py
@@ -183,15 +183,16 @@ def modificarItem():
 
 
 @app.route("/item/datos")
-@app.route("/item/datos/<idItemActual>")
-def datos(idItemActual):
+@app.route("/item/datos/<idItemActual>/<idProyecto>")
+def datos(idItemActual, idProyecto = None):
     '''Se encarga render a un pantalla para cargar datos de item '''
     t = control.getItemById(idItemActual)
-    return render_template('datosItem.html', item = t)
+    return render_template('datosItem.html', item = t, idProyecto =  idProyecto)
 
 #si no funciona poner atributos/tipoItem/nuevo
-@app.route("/item/datos/nuevo", methods=['GET','POST'])
-def nuevaDatosItem():
+@app.route("/item/datos/nuevo")
+@app.route("/item/datos/nuevo/<idProyecto>", methods=['GET','POST'])
+def nuevaDatosItem(idProyecto):
     '''Se encarga de Agregar nuevos datos a un item'''
     version = request.form['version']
     complejidad = request.form['complejidad']
@@ -226,92 +227,53 @@ def nuevaDatosItem():
         
         
 
-    return redirect(url_for('datos',idItemActual = idItemActual))
+    return redirect(url_for('datos',idItemActual = idItemActual, idProyecto = idProyecto))
 
 
 
-@app.route("/item/datos/modificar", methods=['GET','POST'])
-def modificarDatosItem():
-    '''Se encarga de modificar datos de un  item'''
+#@app.route("/item/datos/modificar", methods=['GET','POST'])
+#def modificarDatosItem():
+    
     #version = request.form['version']
     
     
     #complejidad = request.form['complejidad']
     #prioridad = request.form['prioridad']
     #estado = request.form['estado']
-    idItemActual = request.form['idItemActual']
-    idDatosItem = request.form['idItem']
+    #idItemActual = request.form['idItemActual']
+    #idDatosItem = request.form['idItem']
     
     
-    if(idItemActual and idDatosItem):
+    #if(idItemActual and idDatosItem):
         #anga
         
-        datos = controladorDatosItem.getDatosItemById(idDatosItem)
+        #datos = controladorDatosItem.getDatosItemById(idDatosItem)
         #datos.version = version
         #datos.complejidad = complejidad
         #datos.prioridad = prioridad
-        datos.estado = "caducado"
+        #datos.estado = "caducado"
         
         #Aca Modifica los datos viejos 
-        r = controladorDatosItem.modificarDatosItem(datos)
+        #r = controladorDatosItem.modificarDatosItem(datos)
         #aca carga un nuevo dato
         
         
-        if( r["estado"] == True ):
-            flash("Se modifico los datos con exito")
-        else:
-            flash("Ocurrio un error : " + r["mensaje"])
+        #if( r["estado"] == True ):
+            #flash("Se modifico los datos con exito")
+        #else:
+            #flash("Ocurrio un error : " + r["mensaje"])
 
 
-    else :
-        flash("Ocurrio un error, debe completar correctamente el formulario")
+    #else :
+        #flash("Ocurrio un error, debe completar correctamente el formulario")
 
     
-    return redirect(url_for('datos',idItemActual = idItemActual))
-
-
-@app.route("/item/datos/volver", methods=['GET','POST'])
-def volverDatosItem():
-    '''Se encarga de modificar datos de un  item'''
-    version = request.form['version']
-    #complejidad = request.form['complejidad']
-    #prioridad = request.form['prioridad']
-    #estado = request.form['estado']
-    idItemActual = request.form['idItemActual']
-    idDatosItem = request.form['idItem']
-    
-    if(version and idItemActual and idDatosItem):
-        #anga
-        item = control.getItemById(idItemActual)
-        item.ultimaVersion = version
-        p = control.modificarItem(item)
-        
-        #datos.version = version
-        #datos.complejidad = complejidad
-        #datos.prioridad = prioridad
-        datos = controladorDatosItem.getDatosItemById(idDatosItem)
-        datos.estado = "actual"
-        
-        controladorDatosItem.modificarDatosItem(datos)
-        
-        if( p["estado"] == True ):
-            flash("Se volvio a esta version de item")
-        else:
-            flash("Ocurrio un error : " + p["mensaje"])
-
-
-    else :
-        flash("Ocurrio un error, debe completar correctamente el formulario")
-
-    return redirect(url_for('datos',idItemActual = idItemActual))
-
-
-
+    #return redirect(url_for('datos',idItemActual = idItemActual))
 
 
 @app.route("/item/datos/eliminar")
-@app.route("/item/datos/eliminar/<idItemActual>/<idItem>")
-def eliminarDatosItem(idItemActual,idItem):
+@app.route("/item/datos/eliminar/<idItemActual>/<idProyecto>/<idItem>")
+def eliminarDatosItem(idItemActual,idProyecto, idItem ):
     '''Se encarga de eliminar un atributo de un tipo de item'''
     print idItemActual
     print idItem
@@ -330,12 +292,54 @@ def eliminarDatosItem(idItemActual,idItem):
     else:
         flash("Ocurrio un error, intente de nuevo")
 
-    return redirect(url_for('datos', idItemActual= idItemActual))
+    return redirect(url_for('datos', idItemActual= idItemActual, idProyecto = idProyecto))
 
 
-#@app.route("/item/return")
-#@app.route("/item/return/<idItemActual>/<idProyecto>/<idFase>")
-#def returnDatosItem(idItemActual,idProyecto, idFase):
-#    return redirect(url_for('indexItem', idProyecto= idProyecto, idFase=idFase ))
+@app.route("/item/return")
+@app.route("/item/return/<idProyecto>/<idFase>")
+def returnDatosItem(idProyecto, idFase):
+    return redirect(url_for('indexItem', idProyecto = idProyecto, idFase=idFase ))
 
+
+
+@app.route("/item/datos/revertir")
+@app.route("/item/datos/revertir/<idItem>/<idItemActual>/<idProyecto>")
+def revertirDatosItem(idItem= None, idItemActual = None, idProyecto = None ):
+    '''Se encarga de modificar datos de un  dato'''
+    item = control.getItemById(idItemActual)
+    datos = controladorDatosItem.getDatosItemById(idItem)
+    
+    datos.estado = "actual"
+    item.ultimaVersion = datos.version
+        
+    p = control.modificarItem(item)
+    controladorDatosItem.modificarDatosItem(datos)
+        
+    if( p["estado"] == True ):
+        flash("Se volvio a esta version de item")
+    else:
+        flash("Ocurrio un error : " + p["mensaje"])
+
+
+    
+    return redirect(url_for('datos',idItemActual = idItemActual, idProyecto = idProyecto))
+
+
+@app.route("/item/datos/caducar")
+@app.route("/item/datos/caducar/<idItem>/<idItemActual>/<idProyecto>")
+def caducarDatosItem(idItem= None, idItemActual = None, idProyecto = None ):
+    '''Se encarga de modificar datos de un  dato'''
+    
+    datos = controladorDatosItem.getDatosItemById(idItem)
+    datos.estado = "caducado"
+    p= controladorDatosItem.modificarDatosItem(datos)
+        
+    if( p["estado"] == True ):
+        flash("Se volvio a esta version de item")
+    else:
+        flash("Ocurrio un error : " + p["mensaje"])
+
+
+    
+    return redirect(url_for('datos',idItemActual = idItemActual, idProyecto = idProyecto))
 
