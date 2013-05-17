aa303e206af08b31c29e9d19bb396691fde8835e
cathesanz <catakurt@gmail.com>
2013-05-17 12:22:11 -0400

    Test iteración 4

---
+++ sgp/app/testes/lbTest.py
@@ -0,0 +1,60 @@
+import unittest  
+import os
+from app import db
+from app.controlador import ControlLineaBase
+#from app.controlador import ControlRelacion
+
+from app.modelo import LineaBase
+#from app.modelo import Relacion
+# from app.controlador import ControlUsuario
+# from app.modelo import Usuario
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class TestesItCuatro(unittest.TestCase):  
+        def setUp(self):
+            self.controlador = ControlLineaBase()
+            self.lbPrincipal = LineaBase(numero=100, estado=200)
+            self.lbDistinto = LineaBase(numero=200, estado=300)
+            self.lbIgual = LineaBase(numero=100, estado=200)
+            self.contador = 0
+      
+        def testGuarda(self):  
+            r = self.controlador.nuevaLineaBase(self.lbPrincipal)
+            print "Creamos una lb nueva: debe ser cien " + str(self.lbPrincipal.numero)
+            self.assertEqual(r["estado"], True )  
+            self.contador = 1
+            
+        def testGuardaRepetido(self):
+            print "Creamos un usuario nuevo:" + str(self.lbPrincipal.numero)
+            r = self.controlador.nuevaLineaBase(self.lbPrincipal)
+            self.assertEqual(r["estado"], True)
+            print "Creamos un usuario nuevo:" + str(self.lbIgual.numero) 
+            r = self.controlador.nuevaLineaBase(self.lbIgual)
+            self.assertEqual(r["estado"], True) #modifiqué este 
+            print "No deberia guardar porque son iguales" 
+            self.contador = 2
+             
+        def testGuardarDistinto(self):
+            print "Creamos una lb nueva:cien  " + str(self.lbPrincipal.numero)
+            r = self.controlador.nuevaLineaBase(self.lbPrincipal)
+            self.assertEqual(r["estado"], True )
+            print "Creamos una lb nueva: doscientos " + str(self.lbDistinto.numero)
+            r = self.controlador.nuevaLineaBase(self.lbDistinto)
+            self.assertEqual(r["estado"], True )
+            print "Se guardan ambos con exito"
+            self.contador = 3
+            
+        def tearDown(self):  
+            if self.contador == 1 :
+                print "Eliminamos una lb nueva: cien " + str(self.lbPrincipal.numero)
+                self.controlador.eliminarLineaBase(self.lbPrincipal)
+            if self.contador == 2 :
+                print "Eliminamos una lb nueva: cien " + str(self.lbPrincipal.numero)
+                self.controlador.eliminarLineaBase(self.lbPrincipal)
+            if self.contador == 3 :
+                print "Eliminamos una lb nueva: cien " + str(self.lbPrincipal.numero)
+                self.controlador.eliminarLineaBase(self.lbPrincipal)
+                print "Eliminamos una lb nueva: doscientos  " + str(self.lbDistinto.numero)
+                self.controlador.eliminarLineaBase(self.lbDistinto)
---
+++ sgp/app/testes/relacionTest.py
@@ -0,0 +1,60 @@
+import unittest  
+import os
+from app import db
+from app.controlador import ControlRelacion
+from app.modelo import Relacion
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+# idRelacion = db.Column(db.Integer, primary_key = True)
+#     tipo = db.Column( db.String(45), index = True, nullable = False)
+
+
+class TestesRelacion(unittest.TestCase):  
+        def setUp(self):
+            self.controlador = ControlRelacion()
+            self.Principal = Relacion(tipo="Antecesor-Sucesor" , idSucesor=1,  idAntecesor=2)
+            self.Distinto = Relacion(tipo="Antecesor-Sucesor" , idSucesor=1000,  idAntecesor=2000)
+            self.Igual = Relacion(tipo="Antecesor-Sucesor" , idSucesor=1,  idAntecesor=2)
+            self.contador = 0
+      
+        def testGuarda(self):  
+            r = self.controlador.nuevoRelacion(self.Principal)
+            print "Creamos una relacion en test Guarda:  " + str(self.Principal.idSucesor)
+            self.assertEqual(r["estado"], False )  #modifiq este cambiar 
+            self.contador = 1
+            
+        def testGuardaRepetido(self):
+            print "Creamos una relacion en Guarda Repetido: " + str(self.Principal.idSucesor)
+            r = self.controlador.nuevoRelacion(self.Principal)
+            self.assertEqual(r["estado"], False)#modifiq este cambiar 
+            print "Creamos una relacion igual en guarda repetido: " + str(self.Igual.idSucesor) 
+            r = self.controlador.nuevoRelacion(self.Igual)
+            self.assertEqual(r["estado"], False) #modifiq este cambiar 
+            print "No deberia guardar porque son iguales" 
+            self.contador = 2
+             
+        def testGuardarDistinto(self):
+            print "Creamos una relacion principal en guarda distinto  " + str(self.Principal.idSucesor)
+            r = self.controlador.nuevoRelacion(self.Principal)
+            self.assertEqual(r["estado"], False )#este
+            print "Creamos una relacion distinta en guarda distinto " + str(self.Distinto.idSucesor)
+            r = self.controlador.nuevoRelacion(self.Distinto)
+            self.assertEqual(r["estado"], False ) # este
+            print "Se guardan ambos con exito"
+            self.contador = 3
+            
+        def tearDown(self):  
+            if self.contador == 1 :
+                print "Eliminamos la relacion principal " + str(self.Principal.idSucesor)
+                self.controlador.eliminarRelacion(self.Principal)
+            if self.contador == 2 :
+                print "Eliminamos la relacion principal 2 " + str(self.Principal.idSucesor)
+                self.controlador.eliminarRelacion(self.Principal)
+            if self.contador == 3 :
+                print "Eliminamos la relacion principal 3 " + str(self.Principal.idSucesor)
+                self.controlador.eliminarRelacion(self.Principal)
+                print "Eliminamos la relacion distinta   " + str(self.Distinto.idSucesor)
+                self.controlador.eliminarRelacion(self.Distinto)
